698a75f694bfa3900832ad5da8d3bbd64450e069
Marsel <nowly2k@mail.ru>
2023-09-29 15:56:47 +0300

    .

--- src/App.js
+++ src/App.js
@@ -4,22 +4,24 @@ import Header from "./components/Header";
 import Categories from "./components/Categories";
 import Sort from "./components/Sort";
 import PizzaBlock from "./components/PizzaBlock";
+import pizzas from './pizzas.json'
 
 
 function App() {
   return (
-    <div class="wrapper">
+    <div className="wrapper">
       <Header />
-      <div class="content">
-        <div class="container">
-          <div class="content__top">
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
             <Categories />
             <Sort />
           </div>
-          <h2 class="content__title">Все пиццы</h2>
-          <div class="content__items">
-            <PizzaBlock title='Мексиканская' price='399' />
-            <PizzaBlock title='Маргарита' price='299' />
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            {pizzas.map((obj) => (
+              <PizzaBlock key={obj.id} {...obj} />
+            ))}
           </div>
         </div>
       </div>
--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,16 +1,32 @@
-function Categories() {
+import { useState } from "react";
+
+export default function Categories() {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const categories = [
+    "Все",
+    "Мясные",
+    "Вегетарианские",
+    "Гриль",
+    "Острые",
+    "Закрытые",
+  ];
+
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {categories.map((value, index) => {
+          return (
+            <li
+              key={index}
+              onClick={() => setActiveIndex(index)}
+              className={activeIndex === index ? "active" : ""}
+            >
+              {value}
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
 }
-
-export default Categories;
